539299b63129b282c60532c6a581795bcf2f3545
Pavel <duke1715@gmail.com>
2025-04-09 18:54:14 +0300

    Create copy_secrets.sh

---
+++ copy_secrets.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+VAULT_SOURCE="dev/firstSecret"
+VAULT_TARGET="dev/firstSecret"
+
+copy_secrets_recursively () {
+  local source_path="$1"
+  local target_path="$2"
+
+  items=$(vault kv list -format=json "$source_path" | jq -r '.[]')
+
+  for item in $items; do
+    if [[ "$item" == */ ]]; then
+      copy_secrets_recursively "${source_path}/${item%/}" "${target_path}/${item%/}"
+    else
+      echo "Copying secret: ${source_path}/${item} -> ${target_path}/${item}"
+
+      secret_data=$(vault kv get -format=json "${source_path}/${item}" | jq '.data')
+      if [[ $? -ne 0 ]]; then
+        echo "Error retrieving secret ${source_path}/${item}, skipping."
+        continue
+      fi
+
+      echo "$secret_data" | vault kv put "${target_path}/${item}" -
+      if [[ $? -ne 0 ]]; then
+        echo "Error writing secret ${target_path}/${item}, please check permissions."
+      fi
+    fi
+  done
+}
+
+copy_secrets_recursively "$VAULT_SOURCE" "$VAULT_TARGET"
